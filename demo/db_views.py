37995547899f7a6133dd861d8a74f5e94ba15a17
srikanthpragada <srikanthpragada@gmail.com>
2019-02-25 17:59:42 +0530

    25-FEB

--- demo/db_views.py
+++ demo/db_views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from .forms import TitleForm
+from . models import  Title
 
 
 def add_title(request):
     if request.method == "POST":
-
         # send data from request to form
         f = TitleForm(request.POST)
         if f.is_valid():
             f.save()  # insert row into table
             return render(request, 'add_title.html',
                           {'form' : f,
-                           'message' : 'Jobs has been added!'})
+                           'message' : 'Title has been added!'})
     else:  # GET request
         f = TitleForm()
 
     return render(request, 'add_title.html',{'form' : f})
+
+
+def list_titles(request):
+    titles = Title.objects.all()
+    print(titles)
+    return render(request, 'list_titles.html',{'titles' : titles })
--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from . import views, hr_views
+from . import views, hr_views,db_views
 
 urlpatterns = [
    path("welcome/", views.welcome),
@@ -10,5 +10,7 @@ urlpatterns = [
    path("interest/", views.interest),
    path("interest2/", views.interest2),
    path("listjobs/", hr_views.list_jobs),
+   path("addtitle/", db_views.add_title),
+   path("titles/", db_views.list_titles),
 
 ]
